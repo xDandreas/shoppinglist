368d1f5a0d1d6dc9efa0b6b16bec939fb37d0f6c
xDandreas <sinclairdavid094@gmail.com>
2020-09-30 21:06:54 -0500

    Modified Developer Page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'My Supermarket Shopping List',
       theme: ThemeData(
-        appBarTheme: AppBarTheme(color: Color(0xFF151026)),
+        appBarTheme: AppBarTheme(color: Color.fromRGBO(0, 0, 128, 1)),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'My Supermarket Shopping List'),
@@ -214,7 +214,6 @@ class _ListItemState extends State<ListItem> {
                 ),
               ),
             ),
-            // ),
 
             IconButton(
               color: Colors.red,
@@ -238,7 +237,6 @@ class _ListItemState extends State<ListItem> {
                   } else {
                     status = true;
                   }
-
                   _switchTextStyle();
                 })
               },
@@ -248,7 +246,6 @@ class _ListItemState extends State<ListItem> {
                     fontSize: 16.0, decoration: TextDecoration.lineThrough),
               ),
             ),
-            // ),
 
             IconButton(
               color: Colors.red,
@@ -328,139 +325,267 @@ class EnterItems extends StatelessWidget {
 }
 
 class _AboutPage extends StatelessWidget {
+  final String _fullName = "Daavid Sinclair";
+  final String _status = "Software Engineer";
+  final String _bio =
+      "\"I am Daavid A.S. Sinclair. I am a 19 year old male, who is from Jamaica."
+      "I am a senior at NCU. I am pursuing a Batchelors degree in Computer Science where my concentration is Software Engineering.\"";
+  final String _projects = "173";
+  final String _age = "19";
+
+
+  Widget _buildCoverImage(Size screenSize) {
+    return Container(
+      height: screenSize.height / 2.6,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/background.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfileImage() {
+    return Center(
+      child: Container(
+        width: 160.0,
+        height: 160.0,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/dsinclair.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.circular(100.0),
+          border: Border.all(
+            color: Colors.white,
+            width: 4.5,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFullName() {
+    TextStyle _nameTextStyle = TextStyle(
+      //fontFamily: 'Roboto',
+      color: Colors.black,
+      fontSize: 28.0,
+      fontWeight: FontWeight.w700,
+    );
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 3.0),
+      child: Text(
+        _fullName,
+        style: _nameTextStyle,
+      ),
+    );
+  }
+
+  Widget _buildStatus(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 6.0),
+      decoration: BoxDecoration(
+        color: Theme
+            .of(context)
+            .scaffoldBackgroundColor,
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Text(
+        _status,
+        style: TextStyle(
+          //fontFamily: 'Spectral',
+          color: Colors.black,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSkills(String skill) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 6.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Text(
+        skill,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          //fontFamily: 'Spectral',
+          color: Colors.black,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w300,
+
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStatItem(String label, String count) {
+    TextStyle _statLabelTextStyle = TextStyle(
+      //fontFamily: 'Roboto',
+      color: Colors.black,
+      fontSize: 16.0,
+      fontWeight: FontWeight.w200,
+    );
+
+    TextStyle _statCountTextStyle = TextStyle(
+      color: Colors.black54,
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+    );
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          count,
+          style: _statCountTextStyle,
+        ),
+        Text(
+          label,
+          style: _statLabelTextStyle,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSkillItem(String label) {
+    TextStyle _statLabelTextStyle = TextStyle(
+      //fontFamily: 'Roboto',
+      color: Colors.black,
+      fontSize: 16.0,
+      fontWeight: FontWeight.w200,
+    );
+
+    TextStyle _statCountTextStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+    );
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          label,
+          style: _statCountTextStyle,
+        ),
+      ],
+    );
+  }
+
+
+  Widget _buildStatContainer() {
+    return Container(
+      height: 50.0,
+      margin: EdgeInsets.only(top: 18.0),
+      decoration: BoxDecoration(
+        color: Color(0xFFEFF4F7),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          _buildStatItem("Projects", _projects),
+          _buildStatItem("Age", _age),
+        ],
+      ),
+    );
+  }
+  Widget _buildSkillContainer() {
+    return Container(
+      height: 40.0,
+      margin: EdgeInsets.only(top: 2.0),
+      decoration: BoxDecoration(
+        color: Color(0xFFEFF4F7),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          _buildSkillItem("Skills"),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildBio(BuildContext context) {
+    TextStyle bioTextStyle = TextStyle(
+      //fontFamily: 'Spectral',
+      fontWeight: FontWeight.w400, //try changing weight to w500 if not thin
+      fontStyle: FontStyle.italic,
+      color: Color(0xFF799497),
+      fontSize: 16.0,
+    );
+
+    return Container(
+      color: Theme
+          .of(context)
+          .scaffoldBackgroundColor,
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        _bio,
+        textAlign: TextAlign.center,
+        style: bioTextStyle,
+      ),
+    );
+  }
+
+  Widget _buildSeparator(Size screenSize) {
+    return Container(
+      width: screenSize.width / 1.6,
+      height: 2.0,
+      color: Colors.black54,
+      margin: EdgeInsets.only(top: 4.0),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery
+        .of(context)
+        .size;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("About the Developer"),
       ),
       body: Stack(
-        alignment: Alignment.center,
-        children: [
-          CustomPaint(
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-            ),
-            painter: HeaderCurvedContainer(),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(2.0, 25.0, 2.0, 40.0),
-                  child: Text(
-                    'Meet the Developer!',
-                    style: TextStyle(
-                      fontSize: 35.0,
-                      letterSpacing: 1.5,
-                      color: Colors.white,
-                      //fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width / 2,
-                height: MediaQuery.of(context).size.width / 2,
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  image: DecorationImage(
-                    image: AssetImage("assets/dsinclair.png"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 16.0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Card(
-                          child: Column(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text("Daavid Sinclair",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 30.0,
-                                      letterSpacing: 1,
-                                      fontWeight: FontWeight.bold,
-                                    ))
-                              ]),
-                        ),
+        children: <Widget>[
+          _buildCoverImage(screenSize),
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: screenSize.height / 10.4),
+                  Positioned(
+                      child: _buildProfileImage()),
+                  Container(
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        _buildFullName(),
+                        _buildStatus(context),
+                        _buildStatContainer(),
+                        _buildBio(context),
+                        _buildSeparator(screenSize),
+                        _buildSkillContainer(),
+                        _buildSkills("C#, C++, Python, Java, Dart, ASP.net, Flask, PHP, Laravel, Flutter")
                       ],
                     ),
-                  ]),
+                  ),
+                  SizedBox(height: 10.0),
+                  SizedBox(height: 8.0),
+                ],
+              ),
             ),
           ),
-          Row(
-            children: <Widget>[
-              Flexible(
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20.0, horizontal: 16.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          SizedBox(
-                            height: 310.0,
-                          ),
-
-                          //My name is Daavid A.S. Sinclair. I am a 19 year old male, who is from Jamaica.
-                          Text(
-                            'I am Daavid A.S. Sinclair. I am a 19 year old male, who is from Jamaica.\n\n'
-                            'I am a senior at NCU. I am pursuing a Batchelors degree in Computer Science where my concentration is Software Engineering.\n\n'
-                            'I am fluent in Pyton, Java, C# and C++, and I am experienced with Flask, JSP, PHP, ASP.net, Laravel and learning Flutter.',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
-                              letterSpacing: 1.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          )
         ],
       ),
     );
   }
-}
-
-class HeaderCurvedContainer extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()..color = const Color(0xFF151026);
-    Path path = Path()
-      ..relativeLineTo(0, 150)
-      ..quadraticBezierTo(size.width / 2, 250.0, size.width, 150)
-      ..relativeLineTo(0, -150)
-      ..close();
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
